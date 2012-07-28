d2f76014cfe1f8dc7f7ec5c4a3a06060f380615c
Bartłomiej Kiełbasa <kontakt@bkielbasa.pl>
2012-07-28 12:01:23 +0200

    usuwanie grupy

--- application/controllers/GroupController.php
+++ application/controllers/GroupController.php
@@ -4,110 +4,67 @@ require_once 'BaseController.php';
 
 class GroupController extends BaseController
 {
-    function indexAction()
-    {
-        if (!$this->_loggedIn) {
-            $this->_helper->redirector('index', 'index');
-        }
-        
-        if ($this->_config->getConfig('site/type') == 'closed') {
-            if ($this->_userData['creditals'] != 1 && $this->_userData['admin'] != 1) {
-                $this->_helper->redirector('index', 'index');
-            }
-        }
-
-        $groupTable = new Application_Model_Group;
-
-        $this->view->groups = $groupTable->getUserGroups($this->_userData['id']);
-    }
-
-    function newAction()
-    {
-        if (!$this->_loggedIn) {
-            $this->_helper->redirector('index', 'index');
-        }
-        
-        if ($this->_config->getConfig('site/type') == 'closed') {
-            if ($this->_userData['creditals'] != 1 && $this->_userData['admin'] != 1) {
-                $this->_helper->redirector('index', 'index');
-            }
-        }
-
-        $form = new Application_Form_Group_New;
-
-        if (count($_POST)) {
-            if ($form->isValid($_POST)) {
-                $groupTable = new Application_Model_Group;
-
-                $data = array();
-                $data['name'] = $_POST['name'];
-                $data['user'] = $this->_userData['id'];
-
-                $groupTable->insert($data);
-
-                $this->_flashMessenger->setNamespace('success')->addMessage('Grupa została grupa');
-                $this->_helper->redirector('index', 'group');
-            }
-        }
-
-        $this->view->form = $form;
-    }
-
-    public function editAction()
-    {
-        if (!$this->_loggedIn) {
-            $this->_helper->redirector('index', 'index');
-        }
-        
-        if ($this->_config->getConfig('site/type') == 'closed') {
-            if ($this->_userData['creditals'] != 1 && $this->_userData['admin'] != 1) {
-                $this->_helper->redirector('index', 'index');
-            }
-        }
-
-        $group_id = intval($this->_getParam('id'));
-        $groupTable = new Application_Model_Group;
-        $group = $groupTable->find($group_id)->getRow(0)->toArray();
-
-        $form = new Application_Form_Group_New;
-
-        if (count($_POST)) {
-            if ($form->isValid($_POST)) {
-                $data = array(
-                    'name'  => $_POST['name']
-                );
-
-                $groupTable->update($data, 'id = ' . $group_id);
-
-                $this->_flashMessenger->setNamespace('success')->addMessage('Grupa została zachowana');
-                $this->_helper->redirector('index', 'group');
-            }
-        } else {
-            $form->setDefault('name', $group['name']);
-        }
-
-        $this->view->form = $form;
-    }
-
-    public function removeAction()
-    {
-        if (!$this->_loggedIn) {
-            $this->_helper->redirector('index', 'index');
-        }
-        
-        if ($this->_config->getConfig('site/type') == 'closed') {
-            if ($this->_userData['creditals'] != 1 && $this->_userData['admin'] != 1) {
-                $this->_helper->redirector('index', 'index');
-            }
-        }
-
-        $groupTable = new Application_Model_Group;
-
-        $group_id = intval($this->_getParam('id'));
-
-        $groupTable->delete('id = ' . $group_id);
-
-        $this->_flashMessenger->setNamespace('success')->addMessage('Grupa została usunięta!');
-        $this->_helper->redirector('index', 'group');
-    }
+	function indexAction()
+	{
+		$groupTable = new Application_Model_Group;
+		
+		$this->view->groups = $groupTable->getUserGroups($this->_userData['id']);
+	}
+	
+	function newAction()
+	{
+		$form = new Application_Form_Group_New;
+		$groupTable = new Application_Model_Group;
+		
+		if (count($_POST)) {
+			if ($form->isValid($_POST)) {
+				$data = array(
+						'name'	=> $_POST['name'],
+						'user'	=> $this->_userData['id'],
+				);
+				
+				$groupTable->insert($data);
+				
+				$this->_flashMessenger->setNamespace('success')->addMessage('Grupa została dodana');
+				$this->_helper->redirector('index', 'group');
+			}
+		}
+		
+		$this->view->form = $form;
+	}
+	
+	function editAction()
+	{
+		$form = new Application_Form_Group_New;
+		$groupTable = new Application_Model_Group;
+		
+		$group = $groupTable->find($this->_getParam('id'))->getRow(0)->toArray();
+		
+		if (count($_POST)) {
+			if ($form->isValid($_POST)) {
+				$data = array(
+						'name'	=> $_POST['name'],
+						'user'	=> $this->_userData['id'],
+				);
+		
+				$groupTable->update($data, 'id = ' . $group['id']);
+		
+				$this->_flashMessenger->setNamespace('success')->addMessage('Grupa została zaktualizowana');
+				$this->_helper->redirector('index', 'group');
+			}
+		} else {
+			$form->setDefault('name', $group['name']);
+		}
+		
+		$this->view->form = $form;
+	}
+	
+	public function removeAction()
+	{
+		$groupTable = new Application_Model_Group;
+		$groupTable->delete('id = ' . $this->_getParam('id'));
+		
+		$this->_flashMessenger->setNamespace('success')->addMessage('Grupa została usunięta');
+		$this->_helper->redirector('index', 'group');
+	}
 }
